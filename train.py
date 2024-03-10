a5e168de97e824aa65f002bedead136d6ee184a6
tinudin <94966768+tinudin@users.noreply.github.com>
2024-03-10 21:53:43 +0100

    Update train.py
    - added TabularDatasetfactory
    - added train/test split using SKlearn

--- train.py
+++ train.py
@@ -56,12 +56,14 @@ def main():
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
     ds = ### YOUR CODE HERE ###
-    
+    ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.15, random_state=42)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
